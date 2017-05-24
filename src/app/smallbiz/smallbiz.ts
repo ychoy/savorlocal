5c2c0bf6ae8d43ab34148ac3286018483bee4aa2
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-23 22:47:29 -0700

    define smallbiz class

---
+++ src/app/smallbiz/smallbiz.ts
@@ -0,0 +1,13 @@
+export class Smallbiz {
+  _id?: string;
+  name: string;
+	categoryOne: string;
+	categoryTwo: string;
+	categoryThree: string;
+	address: string;
+	phone: string;
+	email: string;
+	website: string;
+	description: string;
+}
+
